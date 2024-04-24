d1a3ddc4acf032f0a8df7460723796709602e351
“Sunit <engineersunit@gmail.com>
2024-04-24 12:48:14 +0530

    Java 23 - Switch for primitive numeric data types

--- src/main/java/org/sun/ghosh/java23/PrimitiveTypesSwitchNumber.java
+++ src/main/java/org/sun/ghosh/java23/PrimitiveTypesSwitchNumber.java
@@ -13,18 +13,24 @@ void main(){
     double compressMe = 100; 
     switch (compressMe) {  // exhaustive switch
         case byte byt ->             // -128 <-> 127
-            System.out.println("Fits into *byte*!");
+            say("Fits into *byte*!");
         case char c when c < 10 ->   // 0 <-> 65_535
-            System.out.println("Value fits into *short* and is less than 10");
+            say("Fits into *char* and is less than 10");
         case short s when s < 100 -> // -32_768 <-> 32_767
-            System.out.println("Fits into *short* and is less than 100");
-        case int i when i > 1_000 -> // -2^31 <-> 2^31 -1
-            System.out.println("Fits into *int* and is less than 1K");
-        case long l when l > 10_000 && l < 1_000_000 -> // -2^63 <-> 2^63 -1
-            System.out.println("Fits into *long* and is between 10K and 1M");
-        case float f ->              // 1.40239846e-45 <-> 3.40282347e+38
-            System.out.println("Fits into *float*");
-        case double d ->      // 4.94065645841246544e-324 <-> 1.79769313486231570e+308
-            System.out.println("Fits into *double*");
+            say("Fits into *short* and is less than 100");
+        case int i when i < 1_000 -> // -2^31 <-> 2^31 -1
+            say("Fits into *int* and is less than 1K");
+        case long l when l > 10_000 && l < 1_000_000 -> 
+                                     // -2^63 <-> 2^63 -1
+            say("Fits into *long* and is between 10K and 1M");
+        case float f -> // 1.40239846e-45 <-> 3.40282347e+38
+            say("Fits into *float*");
+        case double d ->  
+        // 4.94065645841246544e-324 <-> 1.79769313486231570e+308
+            say("Fits into *double*");
     }
+}
+
+say(String s){
+    System.out.println(s);
 }
