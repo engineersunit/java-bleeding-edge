03cb10b0eeeac2825af3781123d27b9f10ce4fcc
Sunit Ghosh <engineersunit@gmail.com>
2024-06-19 11:26:04 +0530

    JEP 476: Module Import Declarations (Preview)

--- src/main/java/org/sun/ghosh/java23/ModuleImportDeclarations.java
+++ src/main/java/org/sun/ghosh/java23/ModuleImportDeclarations.java
@@ -1,30 +1,27 @@
-
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream; 
 /*
  * Importing the java.base module on-demand would give 
  * immediate access to List, Map, Stream, and Path, without having to 
  * manually import java.util on-demand, and java.util.stream on-demand, 
  * and java.nio.file on-demand.
  */
+/*
+ import java.util.Map;
+ import java.util.function.Function;
+ import java.util.stream.Collectors;
+ import java.util.stream.Stream; 
+*/
 
 import module java.base;
 
 void main(){
-    String[] javaPackages = new String[] { "Map", 
-                                           "stream.Stream",
-                                           "function.Function",
-                                           "stream.Collectors"};
-    Map<String, String> packageMap =
+    var javaPackages = new String[] { "Map", 
+                                      "stream.Stream",
+                                      "function.Function",
+                                      "stream.Collectors"};
+    var packageMap =
     Stream.of(javaPackages)
           .collect(Collectors
                     .toMap(s -> "java.util.".concat(s),
                            Function.identity()));
-    log(packageMap);
-}
-
-private void log(Object log){
-    System.out.println(log);
+    print(packageMap);
 }
