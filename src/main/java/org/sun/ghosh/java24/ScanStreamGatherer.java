ebf5ef713605a5bad60d6bddb796f8aecc17a50d
Sunit Ghosh <engineersunit@gmail.com>
2025-03-21 13:39:19 +0530

    Java 24 Stream Gatherers Scan Example

---
+++ src/main/java/org/sun/ghosh/java24/ScanStreamGatherer.java
@@ -0,0 +1,12 @@
+void main(){
+
+ IntStream
+  .range(1, 10)
+  .boxed()
+  .gather(
+    Gatherers
+     .scan(() -> 1, 
+           (fact, next) -> fact * next))
+  .forEach(fact -> println(fact));
+
+}
